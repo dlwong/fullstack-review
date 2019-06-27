87d1643e77f34dcba1ea6f05939b980a32684340
Dorris Wong <pingki@sbcglobal.net>
2019-06-27 15:12:13 -0700

    Fix Search component

--- database/index.js
+++ database/index.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+//create schema
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  username:  String,
+  url: String,
+  forks:   Number
 });
 
+//create model
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (data, callback) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  Repo.insertMany([{username: 'test', url: 'test' , forks: 1},{username: 'test1', url: 'test1' , forks: 1}], (err) => callback(err))
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 let app = express();
+let port = 1128;
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
+
+var Model = require('../helpers/github.js');
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+
+  Model.save(req.body, (err) => {
+    if (err) res.send('not okay')
+    res.send('okay')
+  })
+
 });
 
 app.get('/repos', function (req, res) {
@@ -15,9 +28,7 @@ app.get('/repos', function (req, res) {
   // This route should send back the top 25 repos
 });
 
-let port = 1128;
 
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
-});
+
+
 
